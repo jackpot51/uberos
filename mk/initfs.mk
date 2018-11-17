aaa5d63320b440c5f84b1cbbfb2fd65b1252aa9b
Alexander Theißen <alex@theissen.io>
2018-11-17 21:39:36 +0100

    Build installer consistently with --release
    When creating the initfs the installer was build without
    --release while when creating the filesystem it was build
    with --relase. This patch builds it with --release in all
    cases in order to avoid building it multiple times.

--- mk/initfs.mk
+++ mk/initfs.mk
@@ -3,7 +3,7 @@ build/initfs.tag: initfs.toml prefix
 	rm -rf build/initfs
 	mkdir -p build/initfs
 	export PATH="$(PREFIX_PATH):$$PATH" && \
-	cargo run --manifest-path installer/Cargo.toml -- $(INSTALLER_FLAGS) -c $< build/initfs/
+	cargo run --manifest-path installer/Cargo.toml --release -- $(INSTALLER_FLAGS) -c $< build/initfs/
 	touch $@
 
 build/initfs_live.tag: initfs_live.toml prefix
@@ -11,5 +11,5 @@ build/initfs_live.tag: initfs_live.toml prefix
 	rm -rf build/initfs_live
 	mkdir -p build/initfs_live
 	export PATH="$(PREFIX_PATH):$$PATH" && \
-	cargo run --manifest-path installer/Cargo.toml -- $(INSTALLER_FLAGS) -c $< build/initfs_live/
+	cargo run --manifest-path installer/Cargo.toml --release -- $(INSTALLER_FLAGS) -c $< build/initfs_live/
 	touch $@
